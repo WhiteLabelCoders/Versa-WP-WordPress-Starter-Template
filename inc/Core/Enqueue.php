748bff5793b14c2357ed10f3f2d03167f1953d9b
Mateusz Major <mateusz.major@inspirelabs.pl>
2022-04-13 12:51:56 +0200

    Ability to add and overwrite additional parameters to js files.

--- inc/Core/Enqueue.php
+++ inc/Core/Enqueue.php
@@ -1,6 +1,8 @@
 <?php
 namespace WLC\Core;
 
+use WLC\Views\Page;
+
 trait Enqueue {
 
 	/**
@@ -36,13 +38,14 @@ trait Enqueue {
 	/**
 	 * Wrapper for enqueue script.
 	 *
-	 * @param string $handle
-	 * @param string $file
-	 * @param array $deps
-	 * @param false $version
-	 * @param bool $in_footer
+	 * @param string       $handle     File ID.
+	 * @param string       $file       File name (app.js) if it is placed within app/js directory or whole path to file if in other directory.
+	 * @param string[]     $deps       Array of dependencies like for e.g. array( 'jquery' ).
+	 * @param false|string $version    Script version.
+	 * @param bool         $in_footer  Place script in footer?
+	 * @param string[]     $attributes Array of additional attributes like for e.g. array( 'defer', 'async', crossorigin => 'anonymous' );
 	 */
-	public function enqueue_script( string $handle, string $file, array $deps = array(), $version = false, $in_footer = true ) {
+	public function enqueue_script( string $handle, string $file, array $deps = array(), $version = false, bool $in_footer = true, array $attributes = array() ) {
 		$src = $file;
 		if ( ! filter_var( $file, FILTER_VALIDATE_URL ) ) {
 			$src = get_scripts_uri() . $file;
@@ -60,7 +63,31 @@ trait Enqueue {
 			$version,
 			$in_footer
 		);
+		if ( $attributes ) {
+			add_filter( 'script_loader_tag', function( $filter_src, $filter_handle ) use ( $handle, $attributes ) {
+				if ( $filter_handle === $handle ) {
+
+					foreach( $attributes as $name => $value ) {
+						if ( is_string( $name ) ) {
+							if ( false === strpos( $filter_src, $name ) ) {
+								$filter_src = str_replace( '<script', '<script ' . $name . '="' . $value . '"', $filter_src );
+							} else {
+								$filter_src = preg_replace('\''. $name .'=\\\'.*\\\'\'', $name . '=\''. $value .'\'', $filter_src );
+							}
+						} else {
+							if ( false === strpos( $filter_src, $value ) ) {
+								$filter_src = str_replace( '<script', '<script ' . $value, $filter_src );
+							}
+						}
+					}
+
+					return $filter_src;
+
+				}
+			}, 10, 2 );
+		}
 	}
+
 
 
 
